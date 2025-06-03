dc0fdfd28ff2709aad9ce7551bc28ddbff00a652
Navya Kumar <411navya.kumar@gmail.com>
2025-06-03 15:33:15 +0530

    Create memset-arrays.cpp

---
+++ coding-blocks/class-6/memset-arrays.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int K[5];
+    memset(K, 2, sizeof(K));
+    for (int i=0; i<5; i++){
+        cout << K[i] << " ";
+    }
+    int G[5];
+    memset(G, 2, sizeof(K));
+    for (int i=0; i<5; i++){
+        cout << G[i] << " ";
+    }
+    return 0;
+}
